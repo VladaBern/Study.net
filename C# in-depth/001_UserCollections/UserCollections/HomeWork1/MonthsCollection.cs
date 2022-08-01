c34da6f338db29c18207826d63eb276035576fce
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-01 17:08:39 +0300

    Add class MonthsCollection

---
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork1/MonthsCollection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace HomeWork1
+{
+    internal class MonthsCollection : IMonthsCollection
+    {
+        Months[] months = new Months[]
+        {
+            new Months("January", 31),
+            new Months("February", 28),
+            new Months("March", 31),
+            new Months("April", 30),
+            new Months("May", 31),
+            new Months("June", 30),
+            new Months("July", 31),
+            new Months("August", 31),
+            new Months("September", 30),
+            new Months("October", 31),
+            new Months("November", 30),
+            new Months("December", 31)
+        };
+
+        public Months this[int index]
+        {
+            get { return months[index - 1]; }
+        }
+
+        public IEnumerable<Months> GetByDays(int days)
+        {
+            foreach (var month in months)
+            {
+                if (month.DaysInMonth == days)
+                    yield return month;
+            }
+        }
+    }
+}
