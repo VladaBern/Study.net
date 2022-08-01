9a0c2835574ca32d835ad899bee9ef6543289287
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-01 17:05:40 +0300

    Delete property IndexOfMonth in class Months

--- C# in-depth/001_UserCollections/UserCollections/HomeWork1/Months.cs
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork1/Months.cs
@@ -3,13 +3,11 @@
     internal class Months
     {
         public string NameOfMonth { get; }
-        public int IndexOfMonth { get; }
         public int DaysInMonth { get; }
 
-        public Months(string nameOfMonth, int indexOfMonth, int daysInMonth)
+        public Months(string nameOfMonth, int daysInMonth)
         {
             NameOfMonth = nameOfMonth;
-            IndexOfMonth = indexOfMonth;
             DaysInMonth = daysInMonth;
         }
     }
