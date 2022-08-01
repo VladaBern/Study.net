7772621dee176851ed2779dbf848a1f37315372b
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-01 16:39:56 +0300

    Add interface IMonthCollection

---
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork1/IMonthsCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HomeWork1
+{
+    internal interface IMonthsCollection
+    {
+        Months this[int index] { get; }
+        IEnumerable<Months> GetByDays(int days);
+    }
+}
