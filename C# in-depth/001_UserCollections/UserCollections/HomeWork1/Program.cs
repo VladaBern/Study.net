13cbd4a5e87fd2211c728be056e3a59dcf748702
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-01 17:16:39 +0300

    Add class Program

---
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthsCollection months = new MonthsCollection();
+
+            Console.WriteLine(months[4].NameOfMonth);
+
+            foreach (var item in months.GetByDays(31))
+            {
+                Console.WriteLine($"In {item.NameOfMonth} - {item.DaysInMonth} days");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
