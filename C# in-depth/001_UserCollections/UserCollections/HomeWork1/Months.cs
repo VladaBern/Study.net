6d8b25e39ec187d40f756fb9894e41a03ccd5238
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-01 16:20:01 +0300

    Add class Months

---
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork1/Months.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeWork1
+{
+    internal class Months
+    {
+        public string NameOfMonth { get; }
+        public int IndexOfMonth { get; }
+        public int DaysInMonth { get; }
+
+        public Months(string nameOfMonth, int indexOfMonth, int daysInMonth)
+        {
+            NameOfMonth = nameOfMonth;
+            IndexOfMonth = indexOfMonth;
+            DaysInMonth = daysInMonth;
+        }
+    }
+}
