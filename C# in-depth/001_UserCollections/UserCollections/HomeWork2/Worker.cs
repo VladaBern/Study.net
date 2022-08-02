ac23c3f532dd7218cb2e18e6655a3ddfae7da2fb
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-02 10:30:11 +0300

    Add class Worker

---
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork2/Worker.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeWork2
+{
+    internal class Worker : Citizen
+    {
+        public Worker(string passportNumber, string name, string surname, int age) : base(passportNumber, name, surname, age) { }
+    }
+}
