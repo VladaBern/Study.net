df8726520694ca1f1273e41bcb14283b2d84e89a
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-02 10:28:47 +0300

    Add class Student

---
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork2/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeWork2
+{
+    internal class Student : Citizen
+    {
+        public Student(string passportNumber, string name, string surname, int age) : base(passportNumber, name, surname, age) { }
+    }
+}
