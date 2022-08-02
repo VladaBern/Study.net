563ab04143828d1952fd86cab3a2501688acad00
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-02 12:25:13 +0300

    Add class CitizensCollection

---
+++ C# in-depth/001_UserCollections/UserCollections/HomeWork2/CitizensCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HomeWork2
+{
+    internal class CitizensCollection
+    {
+        List<Citizen> list = new List<Citizen>();
+
+        public int Add(Citizen citizen)
+        {
+            if (list.Contains(citizen))
+            {
+                throw new InvalidOperationException("This item is already in the collection");
+            }
+
+            if (citizen is Pensioner)
+            {
+                int index = 0;
+
+                for (; index < list.Count; index++)
+                {
+                    if (!(list[index] is Pensioner))
+                        break;
+                }
+                list.Insert(index, citizen);
+                return index + 1;
+            }
+            else
+            {
+                list.Add(citizen);
+                return list.Count;
+            }
+        }
+
+        public void Remove()
+        {
+            list.RemoveAt(0);
+        }
+
+        public void Remove(Citizen citizen)
+        {
+            list.Remove(citizen);
+        }
+
+        public bool Contains(Citizen citizen, out int index)
+        {
+            index = list.IndexOf(citizen);
+
+            if (index == -1)
+                return false;
+
+            index++;
+            return true;
+        }
+
+        public Citizen ReturnLast(out int index)
+        {
+            index = list.Count;
+            return list[list.Count - 1];
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+    }
+}
