b61247b878c525554a3ddd8291dc2548d0b372c2
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-05 16:31:03 +0300

    Add class Program

---
+++ C# in-depth/002_SystemCollections/SystemCollections/HomeTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BuyersCollection collection = new BuyersCollection();
+
+            collection.Add("Alex", "Car");
+            collection.Add("Sam", "Flat");
+            collection.Add("Den", "Telephone");
+            collection.Add("Alex", "Flat");
+            collection.Add("Den", "Car");
+            collection.Add("Den", "Telephone");
+            collection.Add("Sam", "Car");
+
+            foreach (var product in collection.GetProductsByBuyer("Den"))
+                Console.WriteLine(product);
+
+            foreach (var name in collection.GetBuyersByProduct("Car"))
+                Console.WriteLine(name);
+
+            Console.ReadKey();
+        }
+    }
+}
