1536068c632cfab268e2c5bbccd284a20518d24d
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-12 15:44:30 +0300

    Add class Program

---
+++ C# in-depth/003_InputOutput/InputOutput/HomeTask1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a command : ");
+            var command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "search":
+                    {
+                        Console.WriteLine("Enter the name of searching file");
+                        string fileName = Console.ReadLine();
+                        Search search = new Search(fileName);
+                        var pathes = search.SearchPath();
+                        if (pathes.Count == 0)
+                        {
+                            Console.WriteLine("File not found");
+                        }
+                        else
+                        {
+                            foreach (var path in pathes)
+                                Console.WriteLine(path);
+                        }                        
+                        break;
+                    }
+                case "read":
+                    {
+                        Console.WriteLine("Enter the path to file for reading");
+                        string filePath = Console.ReadLine();
+                        Read read = new Read(filePath);
+                        Console.WriteLine(read.ReadContent());
+                        break;
+                    }
+                case "compress":
+                    {
+                        Console.WriteLine("Enter the path to file for compressing");
+                        string filePath = Console.ReadLine();
+                        Compress compress = new Compress(filePath);
+                        compress.CompressFile();
+                        Console.WriteLine("Compression completed");
+                        break;
+                    }
+                case "decompress":
+                    {
+                        Console.WriteLine("Enter the path to file for decompressing");
+                        string filePath = Console.ReadLine();
+                        Decompress decompress = new Decompress(filePath);
+                        decompress.DecompressFile();
+                        Console.WriteLine("Decompression completed");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter another command");
+                        break;
+                    }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
