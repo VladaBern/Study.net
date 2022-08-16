43634ebde78bf2208190958073c9a2c6f1619eda
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-16 15:22:01 +0300

    Move code to methods in Program

--- C# in-depth/003_InputOutput/InputOutput/HomeTask1/Program.cs
+++ C# in-depth/003_InputOutput/InputOutput/HomeTask1/Program.cs
@@ -4,6 +4,49 @@ namespace HomeTask1
 {
     internal class Program
     {
+        public static void Search()
+        {
+            Console.WriteLine("Enter the name of searching file");
+            string fileName = Console.ReadLine();
+            Search search = new Search(fileName);
+            var pathes = search.SearchPath();
+            if (pathes.Count == 0)
+            {
+                Console.WriteLine("File not found");
+            }
+            else
+            {
+                foreach (var path in pathes)
+                    Console.WriteLine(path);
+            }
+        }
+
+        public static void Read()
+        {
+            Console.WriteLine("Enter the path to file for reading");
+            string filePath = Console.ReadLine();
+            Read read = new Read(filePath);
+            Console.WriteLine(read.ReadContent());
+        }
+
+        public static void Compress()
+        {
+            Console.WriteLine("Enter the path to file for compressing");
+            string filePath = Console.ReadLine();
+            Compress compress = new Compress(filePath);
+            compress.CompressFile();
+            Console.WriteLine("Compression completed");
+        }
+
+        public static void Decompress()
+        {
+            Console.WriteLine("Enter the path to file for decompressing");
+            string filePath = Console.ReadLine();
+            Decompress decompress = new Decompress(filePath);
+            decompress.DecompressFile();
+            Console.WriteLine("Decompression completed");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a command : ");
@@ -13,45 +56,22 @@ namespace HomeTask1
             {
                 case "search":
                     {
-                        Console.WriteLine("Enter the name of searching file");
-                        string fileName = Console.ReadLine();
-                        Search search = new Search(fileName);
-                        var pathes = search.SearchPath();
-                        if (pathes.Count == 0)
-                        {
-                            Console.WriteLine("File not found");
-                        }
-                        else
-                        {
-                            foreach (var path in pathes)
-                                Console.WriteLine(path);
-                        }                        
+                        Search();                        
                         break;
                     }
                 case "read":
                     {
-                        Console.WriteLine("Enter the path to file for reading");
-                        string filePath = Console.ReadLine();
-                        Read read = new Read(filePath);
-                        Console.WriteLine(read.ReadContent());
+                        Read();
                         break;
                     }
                 case "compress":
                     {
-                        Console.WriteLine("Enter the path to file for compressing");
-                        string filePath = Console.ReadLine();
-                        Compress compress = new Compress(filePath);
-                        compress.CompressFile();
-                        Console.WriteLine("Compression completed");
+                        Compress();
                         break;
                     }
                 case "decompress":
                     {
-                        Console.WriteLine("Enter the path to file for decompressing");
-                        string filePath = Console.ReadLine();
-                        Decompress decompress = new Decompress(filePath);
-                        decompress.DecompressFile();
-                        Console.WriteLine("Decompression completed");
+                        Decompress();
                         break;
                     }
                 default:
