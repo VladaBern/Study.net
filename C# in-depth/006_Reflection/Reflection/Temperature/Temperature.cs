4579b40515d71fa7db8565858bdf6bfcfdc8fe91
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-07 12:35:05 +0300

    Add library Temperature for HomeTask2

---
+++ C# in-depth/006_Reflection/Reflection/Temperature/Temperature.cs
@@ -0,0 +1,22 @@
+﻿namespace Temperature
+{
+    public class Temperature
+    {
+        private double temperature;
+
+        public Temperature(double temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public double ConvertToFahrenheit()
+        {
+            return (temperature * 9 / 5) + 32;
+        }
+
+        public double ConvertToCelsius()
+        {
+            return (temperature - 32) * 5 / 9;
+        }
+    }
+}
