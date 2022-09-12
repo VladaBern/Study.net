e50f663e69c170235b15e4511f1029974c602ec0
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-12 10:50:07 +0300

    Delete comment

--- C# in-depth/007_Attributes/Attributes/HomeTask1/Program.cs
+++ C# in-depth/007_Attributes/Attributes/HomeTask1/Program.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-// Создайте пользовательский атрибут AccessLevelAttribute, позволяющий определить
-// уровень доступа пользователя к системе. Сформируйте состав сотрудников некоторой фирмы
-// в виде набора классов, например, Manager, Programmer, Director. При помощи атрибута
-// AccessLevelAttribute распределите уровни доступа персонала и отобразите на экране
-// реакцию системы на попытку каждого сотрудника получить доступ в защищенную секцию. 
-
 namespace HomeTask1
 {
     internal class Program
