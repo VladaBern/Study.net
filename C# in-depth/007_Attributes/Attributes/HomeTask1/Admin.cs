fd0817af3bf146931a73d401d48c5a69b147471d
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-12 10:38:11 +0300

    Add users

---
+++ C# in-depth/007_Attributes/Attributes/HomeTask1/Admin.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeTask1
+{
+    [AccessLevel(Access.AddUser)]
+    [AccessLevel(Access.DeleteUser)]
+    [AccessLevel(Access.ChangePassword)]
+    [AccessLevel(Access.ChangePosition)]
+    internal class Admin : User
+    {
+        public Admin(string login, string password) : base(login, password) { }
+    }
+}
---
+++ C# in-depth/007_Attributes/Attributes/HomeTask1/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    internal class User
+    {
+        private string login;
+        private string password;
+
+        public User(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+
+        public bool IsValid(string login, string password)
+        {
+            return login == this.login && password == this.password;
+        }
+
+        public bool IsLoginEquel(string login)
+        {
+            return login == this.login;
+        }
+
+        public void ChangePassword(string password)
+        {
+            if (password != null)
+            {
+                this.password = password;
+            }
+            else
+            {
+                Console.WriteLine("Invalid password");
+            }
+        }
+
+        public User ChangePosition(string position)
+        {
+            switch (position)
+            {
+                case "manager":
+                    {
+                        return new Manager(login, password);
+                    }
+                case "programmer":
+                    {
+                        return new Programmer(login, password);
+                    }
+                case "director":
+                    {
+                        return new Director(login, password);
+                    }
+                default:
+                    {
+                        throw new Exception("Invalid position");
+                    }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{login} {password} {this.GetType().Name.ToLower()}";
+        }
+    }
+}
