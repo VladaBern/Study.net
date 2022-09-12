8f5cb7e67bd5739ef26cce106f303a2ee7d9ebe9
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-12 17:09:08 +0300

    Add logic to render attributes

--- C# in-depth/006_Reflection/Reflection/HomeTask1/Program.cs
+++ C# in-depth/006_Reflection/Reflection/HomeTask1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -12,14 +13,57 @@ namespace HomeTask1
             Console.WriteLine("Is fully trusted : {0}", assembly.IsFullyTrusted);
         }
 
-        static void ListAllTypes(Assembly assembly)
+        static void ListAllTypes(Assembly assembly, List<MemberTypes> members)
         {
             Console.WriteLine("Types in {0}:\n", assembly.FullName);
 
             var types = assembly.GetTypes();
 
             foreach (var type in types)
-                Console.WriteLine("Type - {0}", type);
+            {
+                Console.WriteLine("{0}:", type);
+
+                var attributes = type.GetCustomAttributes(true);
+                if (attributes != null && attributes.Length != 0)
+                    Console.WriteLine("\nAttributes:");
+
+                foreach (var attribute in attributes)
+                {
+                    Console.WriteLine(attribute.GetType().Name);
+                }
+
+                foreach (var member in type.GetMembers(BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (members.Contains(member.MemberType))
+                    {
+                        Console.WriteLine($"\n{member.MemberType} - {member.Name}");
+                        var memberAttributes = member.GetCustomAttributes(true);
+                        if (memberAttributes != null && memberAttributes.Length != 0)
+                            Console.WriteLine("\nAttributes:");
+
+                        foreach (var attribute in memberAttributes)
+                        {
+                            Console.WriteLine(attribute.GetType().Name);
+                        }
+                    }
+                }
+            }
+        }
+
+        static List<MemberTypes> ConvertToEnum(string membersLine)
+        {
+            List<MemberTypes> members = new List<MemberTypes>();
+            
+            foreach (var member in membersLine.Split(" "))
+            {
+                object type;
+                if (Enum.TryParse(typeof(MemberTypes), member, true, out type))
+                {
+                    members.Add((MemberTypes)type);
+                }
+            }
+
+            return members;
         }
 
         static void Main()
@@ -29,6 +73,9 @@ namespace HomeTask1
             Console.WriteLine("Enter a path to assembly");
             string path = Console.ReadLine();
 
+            Console.WriteLine("Enter members");
+            string membersLine = Console.ReadLine();                      
+            
             try
             {
                 assembly = Assembly.LoadFrom(path);
@@ -43,7 +90,7 @@ namespace HomeTask1
 
             Console.WriteLine(new string('-', 25));
 
-            ListAllTypes(assembly);
+            ListAllTypes(assembly, ConvertToEnum(membersLine));
 
             Console.ReadKey();
         }
