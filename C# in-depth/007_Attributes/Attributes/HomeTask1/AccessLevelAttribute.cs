718c7b829f97519e34dac15c56dc6e63f0562167
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-12 10:34:54 +0300

    Add attribute AccessLevel

---
+++ C# in-depth/007_Attributes/Attributes/HomeTask1/AccessLevelAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    internal class AccessLevelAttribute : Attribute
+    {
+        public Access Access { get; }
+
+        public AccessLevelAttribute(Access access)
+        {
+            Access = access;
+        }
+    }
+}
