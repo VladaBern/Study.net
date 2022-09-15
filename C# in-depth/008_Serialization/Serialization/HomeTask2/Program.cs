ea194de713696efb6317280c088ac36f9e45cf67
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-15 14:30:40 +0300

    Add project HomeTask2

---
+++ C# in-depth/008_Serialization/Serialization/HomeTask2/Program.cs
@@ -0,0 +1,34 @@
+﻿using HomeTask1;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+// Создайте новое приложение, в котором выполните десериализацию объекта из предыдущего примера.Отобразите состояние объекта на экране. 
+
+namespace HomeTask2
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var stream = new FileStream("D:/Study.net/C# in-depth/008_Serialization/Serialization/HomeTask1/bin/Debug/net5.0/Serialized.xml", FileMode.Open);
+                var deSerializer = new XmlSerializer(typeof(Product));
+                var item = deSerializer.Deserialize(stream) as Product;
+                Console.WriteLine("Object deserialized\n");
+                Console.WriteLine($"Name - {item.name}");
+                Console.WriteLine($"Price = {item.price}");
+                Console.WriteLine($"Color - {item.color}");
+                Console.WriteLine($"Quantity = {item.quantity}");
+                Console.WriteLine($"Total price = {item.total}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
