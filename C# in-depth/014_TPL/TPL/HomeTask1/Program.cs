f9e840a7242d3a0a902a386db98ab6be385815e2
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-10-14 16:54:59 +0300

    Add class Program to HomeTask1

---
+++ C# in-depth/014_TPL/TPL/HomeTask1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[1000000];
+            Random random = new Random();
+
+            Parallel.For(0, array.Length, i => array[i] = random.Next());
+
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+            var oddNumbers = array.AsParallel().Where(element => element % 2 != 0);
+            timer.Stop();
+            Console.WriteLine($"Parallel - {timer.ElapsedTicks}");
+            timer.Reset();
+
+            timer.Start();
+            var odds = array.Where(element => element % 2 != 0);
+            timer.Stop();
+            Console.WriteLine($"Sequence - {timer.ElapsedTicks}");
+
+            Console.ReadKey();
+        }
+    }
+}
