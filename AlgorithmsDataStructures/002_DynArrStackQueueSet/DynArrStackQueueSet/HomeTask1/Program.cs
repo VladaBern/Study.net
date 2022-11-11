7650aa55c74d43a3f4f0c80c9e71c37b7e8c444f
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-11 18:41:24 +0300

    Add class Program to HomeTask1

---
+++ AlgorithmsDataStructures/002_DynArrStackQueueSet/DynArrStackQueueSet/HomeTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArr<int> arr = new DynamicArr<int>();
+
+            for (int i = 1; i <= 4; i++)
+                arr.Add(i);
+
+            Console.WriteLine($"Capacity before removal = {arr.Capacity}");
+
+            arr.RemoveAt(2);
+
+            Console.WriteLine($"Capacity after removal = {arr.Capacity}");
+
+            Console.ReadKey();
+        }
+    }
+}
