90e1e2bbb65800d88ac9803beeb1c531e718a9c6
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-11 18:42:48 +0300

    Add class Program to HomeTask2

---
+++ AlgorithmsDataStructures/002_DynArrStackQueueSet/DynArrStackQueueSet/HomeTask2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue priorityQueue = new PriorityQueue();
+
+            priorityQueue.Enqueue("Run 300 m", Priority.medium);
+            priorityQueue.Enqueue("Lift 70 kg", Priority.high);
+            priorityQueue.Enqueue("Do warm-up", Priority.high);
+            priorityQueue.Enqueue("Drink protein", Priority.low);
+            priorityQueue.Enqueue("Sleep enough", Priority.high);
+            priorityQueue.Enqueue("Drink water", Priority.medium);
+            priorityQueue.Enqueue("Do row", Priority.low);
+
+            Console.WriteLine(priorityQueue.Count);
+            Console.WriteLine();
+
+            Console.WriteLine(priorityQueue.Peek());
+            Console.WriteLine(priorityQueue.Count);
+            Console.WriteLine();
+
+            while (priorityQueue.Count > 0)
+                Console.WriteLine(priorityQueue.Dequeue());
+
+            Console.ReadKey();
+        }
+    }
+}
