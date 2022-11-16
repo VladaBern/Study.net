4cf9b82aa2c2de49569375c88a3b70e0a4afcc60
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-16 15:32:01 +0300

    Add class Program to HomeTask1

---
+++ AlgorithmsDataStructures/003_Sort/Sort/HomeTask1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static int[] ShellSort(int[] array)
+        {
+            if (array.Length <= 1)
+                throw new InvalidOperationException();
+
+            var d = array.Length / 2;
+
+            while (d >= 1)
+            {
+                for (int i = d; i < array.Length; i++)
+                {
+                    int j = i;
+
+                    while (j >= d && array[j - d] > array[j])
+                    {
+                        Swap(ref array[j], ref array[j - d]);
+                        j = j - d;
+                    }
+                }
+
+                d = d / 2;
+            }
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 69, 14, 3, 8, 74, 2, 6, 1, 43, 7 };
+
+            Console.WriteLine("Array before sorting");
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + " ");
+
+            Console.WriteLine();
+
+            array = ShellSort(array);
+
+            Console.WriteLine("Array after Shell sorting");
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
