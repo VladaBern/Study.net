2c4769c2538e0d61e8df54b1ccf4639c7b49c6c7
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-21 13:24:29 +0300

    Add class Program to HomeTask1

---
+++ AlgorithmsDataStructures/004_HashTable_BinaryTree/HashTable_BinaryTree/HomeTask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<string, string> hashtable = new HashTable<string, string>(2);
+            hashtable.Add("Butoreva", "Vasilina");
+            hashtable.Add("Naumovskaya", "Olya");
+            hashtable.Add("Achepovskaya", "Anna");
+            hashtable.Add("Korolev", "Artyom");
+            hashtable.Add("Kireykov", "Hleb");
+            hashtable.Add("Narbut", "Maksim");
+            hashtable.Add("Smolyak", "Dima");
+
+            Console.WriteLine($"There are {hashtable.Count} students");
+
+            foreach (var key in hashtable.GetKey())
+            {
+                Console.WriteLine($"{key} {hashtable.GetValue(key)}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
