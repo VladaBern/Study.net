53a78142e19a8e0fcb48cdc1ec362a74787c8f84
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-21 13:23:35 +0300

    Add class HashTableItem to HomeTask1

---
+++ AlgorithmsDataStructures/004_HashTable_BinaryTree/HashTable_BinaryTree/HomeTask1/HashTableItem.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeTask1
+{
+    internal class HashTableItem<T, V>
+    {
+        public T Key { get; private set; }
+        public V Value { get; private set; }
+
+        public HashTableItem(T key, V value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
