7e0ea16a31c583617334df94637326a81a77c2c5
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-21 17:35:45 +0300

    Add class BinaryTreeNode to HomeTask2

---
+++ AlgorithmsDataStructures/004_HashTable_BinaryTree/HashTable_BinaryTree/HomeTask2/BinaryTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeTask2
+{
+    internal class BinaryTreeNode<TNode> : IComparable<TNode> where TNode : IComparable<TNode>
+    {
+        public BinaryTreeNode<TNode> Left { get; set; }
+        public BinaryTreeNode<TNode> Right { get; set; }
+        public TNode Value { get; private set; }
+
+        public BinaryTreeNode(TNode value)
+        {
+            Value = value;
+        }
+
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+        }
+
+        public int CompareNode(BinaryTreeNode<TNode> other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+    }
+}
