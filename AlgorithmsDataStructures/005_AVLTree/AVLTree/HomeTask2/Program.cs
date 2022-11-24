fdff5e422ff2a7cb497c76acd563ece04d91673e
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-24 17:25:46 +0300

    Add class Program to HomeTask2

---
+++ AlgorithmsDataStructures/005_AVLTree/AVLTree/HomeTask2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Set tree1 = new Set();
+            tree1.Add(7);
+            tree1.Add(2);
+            tree1.Add(3);
+            tree1.Add(4);
+            tree1.Add(5);
+
+            Set tree2 = new Set();
+            tree2.Add(4);
+            tree2.Add(1);
+            tree2.Add(7);
+            tree2.Add(3);
+            tree2.Add(2);
+            tree2.Add(9);
+
+            var dif = tree1.SymmetricDifference(tree2);
+
+            foreach(int i in dif)
+                Console.Write(i + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
