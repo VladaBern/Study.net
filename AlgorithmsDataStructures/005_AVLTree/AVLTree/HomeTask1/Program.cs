17d258bff6d59a02483b96ff954780df1f9ee2e0
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-11-24 14:38:20 +0300

    Add class Program to HomeTask1

---
+++ AlgorithmsDataStructures/005_AVLTree/AVLTree/HomeTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLTree<int> tree = new AVLTree<int>();
+            tree.Add(5);
+            tree.Add(8);
+            tree.Add(2);
+            tree.Add(4);
+
+            tree.Remove(8);
+
+            Console.ReadKey();
+        }
+    }
+}
