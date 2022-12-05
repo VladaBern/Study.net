ebd21c5d13dbc1b8712b98aa8c3900516f3372c7
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-12-05 18:26:24 +0300

    Add class Vertex to HomeTask4

---
+++ AlgorithmsDataStructures/006-8_Graphs/Graphs/HomeTask4/Vertex.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace HomeTask4
+{
+    internal class Vertex
+    {
+        public int Number { get; }
+        private List<Vertex> parents = new List<Vertex>();
+        private List<Vertex> children = new List<Vertex>();
+
+        public Vertex(int number)
+        {
+            Number = number;
+        }
+
+        public bool IsFree => parents.Count == 0;
+
+        public void AddChild(Vertex child)
+        {
+            children.Add(child);
+            child.parents.Add(this);
+        }
+
+        public void Remove()
+        {
+            foreach (Vertex child in children)
+            {
+                child.parents.Remove(this);
+            }
+        }
+
+        public Vertex GetFirstParent()
+        {
+            return parents[0];
+        }
+    }
+}
