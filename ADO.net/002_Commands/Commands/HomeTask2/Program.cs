cab86af61387af2973ea66147dc77a26696e6ec3
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2023-02-01 19:02:07 +0300

    Fix issues in HomeTask2

--- ADO.net/002_Commands/Commands/HomeTask2/Program.cs
+++ ADO.net/002_Commands/Commands/HomeTask2/Program.cs
@@ -1,35 +1,42 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace HomeTask2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string conStr = @"Data Source=DESKTOP-1LFDLAB; Initial Catalog=GrandSlamDB; Integrated Security=True;";
-            SqlConnection sqlConnection = new SqlConnection(conStr);
-
-            Console.WriteLine("Enter playerID");
-            int playerID = int.Parse(Console.ReadLine());
-
-            SqlCommand cmd = new SqlCommand("selectAthlete", sqlConnection) { CommandType = System.Data.CommandType.StoredProcedure };
-
-            cmd.Parameters.AddWithValue("@playerId", playerID);
-
-            sqlConnection.Open();
-
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
+            using (SqlConnection sqlConnection = new SqlConnection(conStr))
             {
-                for (int i = 0; i < reader.FieldCount; i++)
-                    Console.WriteLine("{0}: {1}", reader.GetName(i), reader[i]);
 
-                Console.WriteLine();
+                Console.WriteLine("Enter playerID");
+                int playerID = int.Parse(Console.ReadLine());
+
+                await sqlConnection.OpenAsync();
+
+                SqlCommand cmd = new SqlCommand("selectAthlete", sqlConnection) { CommandType = System.Data.CommandType.StoredProcedure };
+
+                SqlParameter parameter = cmd.Parameters.Add(new SqlParameter("@playerId", SqlDbType.Int));
+                parameter.Direction = System.Data.ParameterDirection.Input;
+                parameter.Value = playerID;
+
+                await cmd.ExecuteNonQueryAsync();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            Console.WriteLine("{0}: {1}", reader.GetName(i), reader[i]);
+
+                        Console.WriteLine();
+                    }
+                }
             }
-
-            sqlConnection.Close();
         }
     }
 }
