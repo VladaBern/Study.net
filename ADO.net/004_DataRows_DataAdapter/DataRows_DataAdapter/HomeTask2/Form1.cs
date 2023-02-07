03a0c3675959c158837e19fd157a564ebf67d6a8
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2023-02-07 19:14:37 +0300

    Add task solving for project HomeTask2

---
+++ ADO.net/004_DataRows_DataAdapter/DataRows_DataAdapter/HomeTask2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace HomeTask2
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=DESKTOP-1LFDLAB;Initial Catalog=GrandSlamDB;Integrated Security=True";
+            string commandString = "SELECT * FROM PlayerInfos";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(commandString, connectionString);
+
+            adapter.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            dataGridView2.DataSource = ds.GetChanges(DataRowState.Modified).Tables[0];
+        }
+    }
+}
