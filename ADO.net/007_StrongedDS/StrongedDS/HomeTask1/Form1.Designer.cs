dfb8a5e5ddb060fbbc5180852f0c9a9a3347f7e9
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2023-02-16 16:55:44 +0300

    Create solution and project HomeTask1 with solved the task from the topic of ADO.net.StrongedDS

---
+++ ADO.net/007_StrongedDS/StrongedDS/HomeTask1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace HomeTask1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.courts = new System.Windows.Forms.DataGridView();
+            this.matches = new System.Windows.Forms.DataGridView();
+            this.playerStats = new System.Windows.Forms.DataGridView();
+            this.players = new System.Windows.Forms.DataGridView();
+            this.playerInfo = new System.Windows.Forms.DataGridView();
+            this.tournaments = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.courts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matches)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerStats)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.players)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tournaments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // courts
+            // 
+            this.courts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.courts.Location = new System.Drawing.Point(12, 53);
+            this.courts.Name = "courts";
+            this.courts.Size = new System.Drawing.Size(240, 150);
+            this.courts.TabIndex = 0;
+            // 
+            // matches
+            // 
+            this.matches.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matches.Location = new System.Drawing.Point(12, 266);
+            this.matches.Name = "matches";
+            this.matches.Size = new System.Drawing.Size(240, 150);
+            this.matches.TabIndex = 1;
+            // 
+            // playerStats
+            // 
+            this.playerStats.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playerStats.Location = new System.Drawing.Point(279, 53);
+            this.playerStats.Name = "playerStats";
+            this.playerStats.Size = new System.Drawing.Size(240, 150);
+            this.playerStats.TabIndex = 2;
+            // 
+            // players
+            // 
+            this.players.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.players.Location = new System.Drawing.Point(279, 266);
+            this.players.Name = "players";
+            this.players.Size = new System.Drawing.Size(240, 150);
+            this.players.TabIndex = 3;
+            // 
+            // playerInfo
+            // 
+            this.playerInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playerInfo.Location = new System.Drawing.Point(548, 53);
+            this.playerInfo.Name = "playerInfo";
+            this.playerInfo.Size = new System.Drawing.Size(240, 150);
+            this.playerInfo.TabIndex = 4;
+            // 
+            // tournaments
+            // 
+            this.tournaments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tournaments.Location = new System.Drawing.Point(548, 266);
+            this.tournaments.Name = "tournaments";
+            this.tournaments.Size = new System.Drawing.Size(240, 150);
+            this.tournaments.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 26);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Courts";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(279, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(240, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Player statistic";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(548, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(240, 26);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Player info";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(12, 240);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(240, 26);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Matches";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(279, 240);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(240, 26);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "Players";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox6.Location = new System.Drawing.Point(548, 240);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(240, 26);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "Tournaments";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tournaments);
+            this.Controls.Add(this.playerInfo);
+            this.Controls.Add(this.players);
+            this.Controls.Add(this.playerStats);
+            this.Controls.Add(this.matches);
+            this.Controls.Add(this.courts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.courts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matches)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerStats)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.players)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tournaments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView courts;
+        private System.Windows.Forms.DataGridView matches;
+        private System.Windows.Forms.DataGridView playerStats;
+        private System.Windows.Forms.DataGridView players;
+        private System.Windows.Forms.DataGridView playerInfo;
+        private System.Windows.Forms.DataGridView tournaments;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
