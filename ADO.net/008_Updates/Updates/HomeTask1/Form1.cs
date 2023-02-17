a7bf28f5265ba69c77f85655e44cb73489f4b243
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2023-02-17 19:39:44 +0300

    Create solution and project HomeTask1 with solving the task from the topic of ADO.net.Updates

---
+++ ADO.net/008_Updates/Updates/HomeTask1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace HomeTask1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=DESKTOP-1LFDLAB;Initial Catalog=University;Integrated Security=True";
+        string commandString = "SELECT * FROM Students";
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        SqlCommandBuilder builder;
+        DataSet students;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+            adapter.SelectCommand = new SqlCommand(commandString, connection);
+            builder = new SqlCommandBuilder(adapter);
+
+            connection.Open();
+
+            students = new DataSet();
+            adapter.Fill(students);
+
+            dataGridView1.DataSource = students.Tables[0];
+
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            builder.GetUpdateCommand();
+            adapter.Update(students);
+
+            connection.Close();
+        }
+    }
+}
