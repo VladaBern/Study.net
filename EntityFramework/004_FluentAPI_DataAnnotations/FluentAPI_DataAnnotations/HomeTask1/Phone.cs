a5b1a9718ec79cb4f75b6811422cb6932cf616ce
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2023-03-03 14:37:12 +0300

    Create solution and project HomeTask1 with solving the task from the topic of EF.FluentAPI_DataAnnotations

---
+++ EntityFramework/004_FluentAPI_DataAnnotations/FluentAPI_DataAnnotations/HomeTask1/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeTask1
+{
+    public class Phone
+    {
+        public int PhoneId { get; set; }
+
+        [Required, MaxLength(20)]
+        public string BrandName { get; set; }
+
+        [MaxLength(30)]
+        public string PhoneModel { get; set; }
+    }
+}
